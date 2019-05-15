4081c91fb32866a8ba8e528367c5ec3e9b6d984e
davejbarnes <davejbarnes@gmail.com>
2019-05-15 19:17:19 +0100

    Added GoDaddy DNS updater python script

---
+++ gd_dns/gd_dns.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import requests, json
+import gd_dns_config as config
+
+godaddy_headers = {"Authorization":" sso-key " + config.godaddy_key + ":" + config.godaddy_secret}
+
+def get_current_record(domain: str, record_name: str, record_type: str = "A") -> [str, bool]:
+    req = requests.get(config.godaddy_apiurl 
+                        + domain + "/records/" + record_type + "/" + record_name,
+                        headers=godaddy_headers)
+    js = json.loads(req.text)
+    if req.status_code == 200:
+        if len(req.text) > 3:
+            return js[0]["data"], True
+        else:
+            return config.record_name + "." + config.godaddy_domain + " not found", False
+    return js["code"], False
+
+
+def get_currentip() -> str:
+    req = requests.get("http://ipinfo.io/json")
+    ip = req.json()["ip"]
+    return ip
+
+
+def set_record(domain: str, record_name: str, record_type: str, data: str) -> bool:
+    content = [{"data": data, "name": record_name, "ttl": 36000, "type": record_type}]
+    req = requests.put(config.godaddy_apiurl 
+                        + domain + "/records/" + record_type + "/" + record_name,
+                        headers=godaddy_headers, json=content )
+    if req.status_code == 200:
+        return True
+    return False
+
+
+wan_ip = get_currentip()
+current_dns = get_current_record(config.godaddy_domain, config.record_name, "A")
+
+if current_dns[1] == False:
+    print("An error occured:", current_dns[0])
+    exit(1)
+
+if wan_ip != current_dns[0]:
+    print("DNS record is out of date ( WAN =", wan_ip, ", DNS =", current_dns[0], ")")
+    print("Updating record: " + config.record_name + "." + config.godaddy_domain)
+    if(set_record(config.godaddy_domain, config.record_name, "A", wan_ip)):
+        print("Updated successfully")
+        exit(0)
+    else:
+        print("Failed to update")
+        exit(1)
+else:
+    print("All up to date")
---
+++ gd_dns/gd_dns_config.py
@@ -0,0 +1,8 @@
+# Config for gd_dns
+
+godaddy_domain = "your.domain"
+record_name = "record_name"  # such as www, mail etc.
+
+godaddy_key = "your_GoDaddy_developer_production_api_key"
+godaddy_secret = "your_GoDaddy_developer_production_api_secret"
+godaddy_apiurl = "https://api.godaddy.com/v1/domains/"
