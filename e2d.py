8b0119bb45a71825edb9cad628102638d3bf57b2
davejbarnes <davejbarnes@gmail.com>
2019-07-07 11:23:47 +0100

    Added e2d.py - replaces unix timestamp in input line(s), outputs result(s)

---
+++ e2d.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import sys, select, re
+from datetime import datetime
+epoch_regex = "[0-9]{10}"
+rawdata = ""
+if select.select([sys.stdin,],[],[],0.0)[0]:
+    for line in sys.stdin:
+        pattern = re.compile(epoch_regex)
+        pattern_match = pattern.findall(line)
+        if len(pattern_match) > 0:
+            for epochstr in pattern_match:
+                datetime = datetime.fromtimestamp(float(epochstr))
+                line = line.replace(epochstr, str(datetime))
+            print(line, end = '')
+        else:
+            print(line, end = '')
+else:
+    print("No input")
+    exit(1)
