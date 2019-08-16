9feff0f8f35c0ecfab33bdba971c5195cf8e1a5c
davejbarnes <davejbarnes@gmail.com>
2019-08-16 08:08:18 +0100

    Updated how stdin is read

--- e2d.py
+++ e2d.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-import sys, select, re
+import fileinput, re
 from datetime import datetime
+
 epoch_regex = "(?:\b|\D)([0-9]{10})(?:\b|\D)"
-rawdata = ""
-if select.select([sys.stdin,],[],[],0.0)[0]:
-    for line in sys.stdin:
+with fileinput.input('-') as lines:
+    for line in lines:
         pattern = re.compile(epoch_regex)
         pattern_match = pattern.findall(line)
         if len(pattern_match) > 0:
@@ -15,6 +15,3 @@ if select.select([sys.stdin,],[],[],0.0)[0]:
             print(line, end = '')
         else:
             print(line, end = '')
-else:
-    print("No input")
-    exit(1)
